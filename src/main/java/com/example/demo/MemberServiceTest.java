f73bb66c25f35aa4800476c7381f48c128c80d44
NEROAT_73 <kikahi@naver.com>
2022-12-28 22:10:32 +0900

    기본적인 자바 문법을 통해 인터페이스와 클래스의 상호작용 연습
    @Test 사용

---
+++ src/main/java/com/example/demo/MemberServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    public void join(){
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
